83713149666c3b1d0bd40781bf0810c87e2b26a7
codeFabFit <fabgar14@gmail.com>
2023-11-25 16:28:32 -0500

    javascript set up

---
+++ script.js
@@ -0,0 +1,58 @@
+window.addEventListener("DOMContentLoaded", () => {
+const tiles = Array.from(document.querySelectorAll('.tile'));
+const playerDisplay = document.querySelector('.display-player');
+const resetButton = document.querySelector('#reset');
+const announcer = document.querySelector('.announcer')
+
+
+let board = ['','','','','','','','',''];
+let currentPlayer = 'X';
+let isGameActive = true;
+
+const PLAYERX_WON = 'PLAYERX_WON'
+const PLAYERO_WON = 'PLAYERO_WON'
+const TIE = "TIE"
+
+/* 
+ indexes witin the board 
+
+ [0][1][2]
+ [3][4][5]
+ [6][7][8]
+
+*/
+
+// winning conditions 
+
+const winningConditions = [
+
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+
+];
+
+// user action
+
+const userAction=(tile, index)=> {
+    if(isValidAction(tile) && isGameActive) {
+        tile.innerText = currentPlayer;
+        tile.classList.add(`player${currentPlayer}`);
+        updateBoard(index);
+        handleResultValidation();
+        changePlayer();
+    }
+}
+
+
+// tile functions 
+tiles.forEach( (tile, index) => {
+    tile.addEventListener('click', () => userAction(tile, index))
+})
+
+    resetButton.addEventListener('click', resetBoard);
+})
